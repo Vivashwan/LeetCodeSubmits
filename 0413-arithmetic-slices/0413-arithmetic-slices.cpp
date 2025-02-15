fcfdd590fe99d8e2e0131e337b52f0434fe0c9ee
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-02-15 16:52:22 +0530

    Time: 39 ms (5.02%), Space: 152.9 MB (5.94%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -1,13 +1,17 @@
 class Solution {
 private:
-    int func(vector<int>&nums, int n, int curr, int prev, int diff)
+    int func(vector<int>&nums, int n, int curr, int prev, int diff, vector<vector<int>>&dp)
     {
+        if(dp[curr][prev]!=-1)
+        {
+            return dp[curr][prev];
+        }
         if(curr>=n || nums[curr]-nums[prev]!=diff)
         {
             return 0;
         }
 
-        return 1+func(nums, n, curr+1, curr, diff);
+        return dp[curr][prev] = 1+func(nums, n, curr+1, curr, diff, dp);
     }
 
 public:
@@ -21,10 +25,12 @@ public:
 
         int count=0;
 
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+
         for(int i=0; i<n-2; i++)
         {
             int diff=nums[i+1]-nums[i];
-            count+=func(nums, n, i+2, i+1, diff);
+            count+=func(nums, n, i+2, i+1, diff, dp);
         }
 
         return count;
