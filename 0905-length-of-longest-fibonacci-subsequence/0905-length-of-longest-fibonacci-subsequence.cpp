58c290af07647f97895ced3d506d59d0badee794
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-12 22:28:48 +0530

    Time: 149 ms (79.36%), Space: 149.9 MB (26.61%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,52 +1,32 @@
 class Solution {
-private:
-    int func(vector<int>&arr, unordered_map<int, int>&mp, int i, int j, vector<vector<int>>&dp, int n)
-    {
-        if(i>=n || j>=n)
-        {
-            return 0;
-        }
-
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-
-        int res=0;
-
-        int next=arr[i]+arr[j];
-
-        if(mp.find(next)!=mp.end())
-        {
-            int nextJumpIndex=mp[next];
-            res=1+func(arr, mp, j, nextJumpIndex, dp, n);
-        }
-
-        return dp[i][j]=res;
-    }
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
-        int n = arr.size();
-
+        int n=arr.size();
         unordered_map<int, int>mp;
 
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++) 
         {
             mp[arr[i]]=i;
         }
 
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 2)); 
 
         int res=0;
 
-        for(int i=0; i<n-1; i++)
+        for(int j=1; j<n; j++) 
         {
-            for(int j=i+1; j<n; j++)
+            for(int i=0; i<j; i++)
             {
-                res=max(res, 2+func(arr, mp, i, j, dp, n));
+                int expectedPrev=arr[j]-arr[i];
+
+                if(expectedPrev<arr[i] && mp.find(expectedPrev)!=mp.end()) 
+                {
+                    int k = mp[expectedPrev];
+                    dp[i][j] = dp[k][i] + 1;
+                    res = max(res, dp[i][j]); 
+                }
             }
         }
-
-        return res>2?res:0;
+        return res>2 ? res:0;
     }
 };
