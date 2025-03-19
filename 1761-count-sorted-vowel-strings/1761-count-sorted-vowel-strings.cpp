39252194d9ccacadbab0bc87f073fe81292a0663
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-03-19 23:06:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (62.02%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int countVowelStrings(int n) {
         int a=1, e=1, i=1, o=1, u=1;
-        
-        while(--n)
+        n=n-1;
+        while(n--)
         {
             o += u;
             i += o;
