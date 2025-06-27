2f898c522225cb4722294dfa09e5d012d5f187e0
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-06-28 00:18:52 +0530

    Time: 0 ms (100%), Space: 17.1 MB (80.3%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,37 +1,41 @@
 class Solution {
 private:
-    int countOfEleLesserOrEqualTo(vector<vector<int>>& matrix, int x)
+    int midIsGreaterThan(int mid, vector<vector<int>>& matrix, int n)
     {
-        int n = matrix.size(), m = matrix[0].size();
-        int cnt = 0, c = n - 1; // start with the rightmost column
-        for (int r = 0; r < m; ++r) {
-            while (c >= 0 && matrix[r][c] > x) --c;  // decrease column until matrix[r][c] <= x
-            cnt += (c + 1);
-        }
-        return cnt;
-    }
+        int row=0, col=n-1, count=0;
 
+        while(row<n && col>=0)
+        {
+            if(matrix[row][col]<=mid)
+            {
+                count+=(col+1);
+                row++;
+            }
+            else col--;
+        }
+
+        return count;
+    }
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int n=matrix.size();
 
-        int ans = 0;
+        int low=matrix[0][0], high=matrix[n-1][n-1];
 
-        int low = matrix[0][0], high = matrix[n-1][m-1];
+        int res=0;
 
         while(low<=high)
         {
-            int mid = low + (high-low)/2;
+            int mid=low+(high-low)/2;
 
-            if(countOfEleLesserOrEqualTo(matrix, mid)>=k)
+            if(midIsGreaterThan(mid, matrix, n)>=k)
             {
-                ans = mid;
-                high = mid-1;
+                res=mid;
+                high=mid-1;
             }
-            else low = mid+1;
+            else low=mid+1;
         }
-
-        return ans;
+        
+        return res;
     }
 };
