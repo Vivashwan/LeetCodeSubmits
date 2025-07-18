9725d76a6597e9305246947511291675132cc181
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-07-19 00:33:46 +0530

    Time: 7 ms (96.73%), Space: 85 MB (76.1%) - LeetHub

--- 1814-jump-game-vi/1814-jump-game-vi.cpp
+++ 1814-jump-game-vi/1814-jump-game-vi.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> dp(n, INT_MIN); 
-        deque<int> dq; 
-        
-        dp[0] = nums[0];  
-        dq.push_back(0); 
+        int n=nums.size();
 
-        for (int i = 1; i < n; i++) 
+        vector<int>dp(n);
+
+        deque<int>dq;
+
+        dp[0]=nums[0];
+        dq.push_back(0);
+
+        for(int i=1; i<n; i++)
         {
-            if (!dq.empty() && dq.front() < i - k) {
+            while(!dq.empty() && dq.front()<i-k)
+            {
                 dq.pop_front();
             }
 
-            dp[i] = nums[i] + dp[dq.front()];
+            dp[i]=dp[dq.front()]+nums[i];
 
-            while (!dq.empty() && dp[i] >= dp[dq.back()]) {
+            while(!dq.empty() && dp[i]>=dp[dq.back()])
+            {
                 dq.pop_back();
             }
 
             dq.push_back(i);
         }
 
-        return dp[n - 1];
+        return dp[n-1];
     }
 };
