863f90bcb8efcc4d408551e74a4936009aab640c
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-07-28 02:03:53 +0530

    Time: 0 ms (100%), Space: 14.1 MB (60.91%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
-    void func(vector<vector<int>>&ans, int target, int sum, int ind, vector<int>&temp, vector<int>&candidates, vector<bool>&visited)
+    void backtrack(vector<vector<int>>& ans, vector<int>& temp, vector<int>& candidates, int target, int start) 
     {
-        if(sum>target)
-        {
-            return;
-        }
-
-        if(sum==target)
+        if(target==0) 
         {
             ans.push_back(temp);
             return;
         }
 
-        for(int i=ind; i<candidates.size(); i++) 
+        for(int i=start; i<candidates.size(); i++) 
         {
-            if(i>ind && candidates[i]==candidates[i-1])
-            {
+            if(i>start && candidates[i]==candidates[i-1])
+            { 
                 continue;
             }
 
-            if(!visited[i])
-            {
-                sum+=candidates[i];
-                temp.push_back(candidates[i]);
-                visited[i]=true;
-
-                func(ans, target, sum, i+1, temp, candidates, visited);
-                
-                temp.pop_back();
-                visited[i]=false;
-                sum-=candidates[i];
+            if(candidates[i]>target)
+            { 
+                break;
             }
+
+            temp.push_back(candidates[i]);
+
+            backtrack(ans, temp, candidates, target-candidates[i], i+1);
+
+            temp.pop_back();
+        }
     }
-}
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
-
         vector<int>temp;
-        vector<bool>visited(candidates.size(), false);
 
         sort(candidates.begin(), candidates.end());
 
-        func(ans, target, 0, 0, temp, candidates, visited);
+        backtrack(ans, temp, candidates, target, 0);
 
         return ans;
     }
