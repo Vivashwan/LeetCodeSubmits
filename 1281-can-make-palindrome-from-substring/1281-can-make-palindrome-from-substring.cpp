7c488990e15b7bfd0e47949b20b0cabc0750ce97
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-16 02:30:45 +0530

    Time: 139 ms (30.72%), Space: 157.1 MB (23.86%) - LeetHub

--- 1281-can-make-palindrome-from-substring/1281-can-make-palindrome-from-substring.cpp
+++ 1281-can-make-palindrome-from-substring/1281-can-make-palindrome-from-substring.cpp
@@ -7,48 +7,35 @@ public:
 
         for(int i=0; i<n; i++)
         {
+            prefix[i+1]=prefix[i];
             prefix[i+1][s[i]-'a']++;
-
-            for(int j=0; j<26; j++)
-            {
-                prefix[i+1][j]+=prefix[i][j];
-            }
         }
 
         vector<bool>res;
 
-        for(auto it: queries)
+        for(auto it: queries) 
         {
             int left=it[0], right=it[1], k=it[2];
 
-            if(right-left+1<=k)
+            int odd=0;
+
+            for(int i=0; i<26; i++) 
             {
+                int count=prefix[right+1][i]-prefix[left][i];
+
+                if(count%2 != 0) 
+                {
+                    odd++;
+                }
+            }
+
+            if(odd/2<=k)
+            { 
                 res.push_back(true);
             }
-            else
-            {
-                int odd=0;
-
-                for(int i=0; i<26; i++)
-                {
-                    int count=prefix[right+1][i]-prefix[left][i];
-
-                    if(count%2!=0)
-                    {
-                        odd++;
-                    }
-                }
-
-                odd-=2*k;
-
-                if(odd>1)
-                {
-                    res.push_back(false);
-                }
-                else res.push_back(true);
-            }
+            else res.push_back(false);
         }
-        
+
         return res;
     }
 };
