3421c2c0a98e42ebbf252419b8769d5f538a4b7a
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-08-28 22:03:53 +0530

    Time: 86 ms (97.21%), Space: 83.9 MB (69.08%) - LeetHub

--- 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
+++ 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int maxNonOverlapping(vector<int>& nums, int target) {
-        int ans=0, sum=0;
+        int n=nums.size();
+
+        int sum=0, count=0;
 
         unordered_set<int>s;
 
         s.insert(0);
 
-        for(auto it: nums)
+        for(int i=0; i<n; i++)
         {
-            sum+=it;
-
-            if(s.find(sum-target)!=s.end())
+            sum+=nums[i];
+            if(s.count(sum-target))
             {
-                ans++, sum=0;
-
-                s.clear();
-
-                s.insert(0);
+                count++, sum=0;
+                s.clear();  s.insert(0);
             }
             else s.insert(sum);
         }
 
-        return ans;
+        return count;
     }
 };
