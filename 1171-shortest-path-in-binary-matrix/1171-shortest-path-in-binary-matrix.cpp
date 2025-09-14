ac7cd10d58c5e7dd9471470a6bbcc445b1c49993
Vivashwan Ghosh <98079009+Vivashwan@users.noreply.github.com>
2025-09-14 16:08:17 +0530

    Time: 11 ms (90.25%), Space: 23.3 MB (98.59%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,49 +1,51 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
-        int n=g.size();
-        if(g[0][0] || g[n-1][n-1])
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+
+        if(grid[0][0]==1 || grid[n-1][n-1]==1)
         {
             return -1;
         }
 
-        g[0][0]=1;
+        queue<pair<int, int>>q;
 
-        queue<pair<int,int>>q;
-
-        q.push({0,0});
+        grid[0][0]=1;
+        q.push({0, 0});
 
         int count=0;
 
-        while(q.size())
+        while(!q.empty())
         {
-            int k=q.size();
+            int size=q.size();
             count++;
 
-            while(k--)
+            for(int i=0; i<size; i++)
             {
-                int x=q.front().first;
-                int y=q.front().second;
-                q.pop();
+                auto [x, y]=q.front(); q.pop();
+
                 if(x==n-1 && y==n-1)
                 {
                     return count;
                 }
+
                 int dx[8]={1,-1,0,0,1,1,-1,-1};
                 int dy[8]={0,0,1,-1,1,-1,1,-1};
 
-                for(int i=0;i<8;i++)
+                for(int j=0; j<8; j++)
                 {
-                    int nx=x+dx[i];
-                    int ny=y+dy[i];
-                    if(nx>=0 && nx<n && ny>=0 && ny<n && g[nx][ny]==0)
+                    int newX=x+dx[j], newY=y+dy[j];
+
+                    if(newX>=0 && newX<n && newY>=0 && newY<n && grid[newX][newY]==0)
+
                     {
-                        g[nx][ny]=1;
-                        q.push({nx,ny});
+                        grid[newX][newY]=1;
+                        q.push({newX, newY});
                     }
                 }
             }
         }
+
         return -1;
     }
 };
